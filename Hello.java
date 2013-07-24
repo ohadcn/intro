fdff3dfb46a1790f057c748af535425f216c5366
ohad <ohadcn@gmail.com>
2013-07-24 13:14:04 +0300

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+* file:Hello.java
+* @author Ohad Cohen, ohad.cohen6@mail.huji.ac.il
+* description: a simple program that prints "hello world" to the system output
+* and exit, the file is here to help github identify the languege used in this repo
+*/
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
